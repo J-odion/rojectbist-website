5dae372ad9c1caacf704523be93d0b1d0e718b22
J-odion <emmyjaff22@gmail.com>
2023-08-21 02:06:41 +0100

    initialised website

---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home(props) {
+    return (
+        <div className='text-3xl'>
+            Home page is working
+        </div>
+    );
+}
+
+export default Home;
